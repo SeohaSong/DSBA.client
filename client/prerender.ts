6fab54c6bd904eb265e487ef691c8902475d514a
seohasong <tisutoo@gmail.com>
2018-04-15 21:04:39 +0900

    update origin master

---
+++ client/prerender.ts
@@ -0,0 +1,35 @@
+import 'zone.js/dist/zone-node';
+import 'reflect-metadata';
+import {readFileSync, writeFileSync, existsSync, mkdirSync} from 'fs';
+import {join} from 'path';
+
+import {enableProdMode} from '@angular/core';
+enableProdMode();
+
+import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
+import {renderModuleFactory} from '@angular/platform-server';
+import {ROUTES} from './static.paths';
+
+const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./dist/server/main.bundle');
+
+const BROWSER_FOLDER = join(process.cwd(), 'browser');
+
+const index = readFileSync(join('browser', 'index.html'), 'utf8');
+
+let previousRender = Promise.resolve();
+
+ROUTES.forEach(route => {
+  var fullPath = join(BROWSER_FOLDER, route);
+
+  if(!existsSync(fullPath)){
+    mkdirSync(fullPath);
+  }
+
+  previousRender = previousRender.then(_ => renderModuleFactory(AppServerModuleNgFactory, {
+    document: index,
+    url: route,
+    extraProviders: [
+      provideModuleMap(LAZY_MODULE_MAP)
+    ]
+  })).then(html => writeFileSync(join(fullPath, 'index.html'), html));
+});
---
+++ client/static.paths.ts
@@ -0,0 +1,10 @@
+export const ROUTES = [
+  '/',
+  '/main',
+  '/member',
+  '/member/overall',
+  '/member/professor',
+  '/member/students',
+  '/member/alumni',
+  '/research'
+];
