7297bbe69538566156f5b4fcdea71822bcd94603
seohasong <tisutoo@gmail.com>
2018-07-12 16:30:24 +0900

    update origin master

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { DisplayService } from "../_services/display.service";
+import { UtilsService } from "../_services/utils.service";
 
-
-
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-board',
@@ -15,27 +14,28 @@ export class BoardComponent implements OnInit {
 
   constructor(
     private http: HttpClient,
-    private displayService: DisplayService
+    private displayService: DisplayService,
+    private utilsService: UtilsService
   ) { }
 
-  newses: any;
+  post_groups: any;
 
   ngOnInit() {
     this.displayService.set_board_display();
     this.get_newses().subscribe(data => {
-      this.newses = data
+      let posts = data.reverse();
+      this.post_groups = this.utilsService.group_list(8, posts)
     })
   }
-
+  // https://angularfirebase.com/snippets/how-to-reverse-an-observable-array-in-angular-angularfire2-firebaselistobservable/
   safe_html(html){
-    return html.replace(/<.+?>/g, ' ').slice(0, 128)+' ...'
+    return html.replace(/<.+?>/g, ' ').slice(0, 128)+' ...';
   }
 
   get_newses() {
-    let newses = this.http.get('http://localhost:4200/assets/db/news.json')
-    return newses
+    let newses = this.http.get('/assets/db/news.json');
+    return newses;
   }
-
 
 
 }
