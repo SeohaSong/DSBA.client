064c2cbbf851d0f6fb05be42b1ce3ec80d1f1173
seohasong <tisutoo@gmail.com>
2018-12-03 03:39:41 +0900

    update origin master

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -2,7 +2,6 @@ firebase.initializeApp({
   apiKey: 'AIzaSyDXZMi6ZfLPtUB8f2yoU50J8VuT86c2DbQ',
   authDomain: 'dsba-142b3.firebaseapp.com',
   projectId: 'dsba-142b3',
-  databaseURL: 'https://dsba-142b3.firebaseio.com',
   storageBucket: 'gs://dsba-142b3.appspot.com'
 });
 
